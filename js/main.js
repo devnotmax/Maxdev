e910a4f61e84f78de9bd74815284c10aee8a4981
devnotmax <develop.maxsj@gmail.com>
2022-04-25 07:07:49 -0300

    Finished

--- js/main.js
+++ js/main.js
@@ -3,3 +3,32 @@ let menu = document.querySelector('.menu-icon');
 menu.onclick = () => {
     menu.classList.toggle('move')
 }
+
+
+/* Email js */
+function validate() {
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".email");
+    let msg = document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value == ""){
+            emptyerror();
+        } else{
+            sendmail (name.value, email.value, msg.value);
+            success();
+        }
+    });
+}
+validate();
+
+function emptyerror() {
+    Swal.fire({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'Cool'
+      })
+}
